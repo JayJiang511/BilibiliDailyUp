09fdb1b7b862f865212af6179cd35387b4531e1b
JayJiang <104381501+JayJiang511@users.noreply.github.com>
2025-06-22 11:36:58 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -30,8 +30,7 @@ UID_LIST = ['473837611', '1131457022', '433587902', '2026561407', '50329118']
 # 获取UID的方法见README.md
 
 COOKIE_LIST = [
-    r"
-buvid3=4E9A88EC-4D42-B097-D5AA-BF90BBFBFFC040004infoc; b_nut=1744143440; _uuid=DA2E11010E-103E3-267F-C43C-8F34CD2106CD371537infoc; enable_web_push=DISABLE; enable_feed_channel=ENABLE; buvid4=34361D6A-6EC4-32D9-F338-3C6AF3DCB15041672-025040820-AtKSaKPJ4Zt08EyrTaEZqA%3D%3D; rpdid=|(uR)m~u|k)R0J'u~RRuRJlYJ; DedeUserID=3461569495173335; DedeUserID__ckMd5=5b1e7cafee152dfb; hit-dyn-v2=1; buvid_fp_plain=undefined; LIVE_BUVID=AUTO7917452335374594; CURRENT_QUALITY=80; bp_t_offset_3461569495173335=1071686424450826240; SESSDATA=5d098847%2C1764185617%2Ce40ef%2A51CjD_aJQYfysyEWz734AXsXZZNfUUGHjmeNrqZefHvJ9Rz5WDNlNRh5oYCfuyQ0dmw0oSVmhSTTBpZHFRZFNtSnp1bS1xcTRDcU1GZzhpTjNiY0oxY3ZBWHBpZVVVMm5kS2E3MnFXUS1JR3BmRllKemJabmU3TlBTYnk4RmhlYXFyN0UzNFV4bjV3IIEC; bili_jct=b1c3e80e7182938dd0ac877ec770c945; header_theme_version=OPEN; theme-tip-show=SHOWED; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTA3MDU0MzIsImlhdCI6MTc1MDQ0NjE3MiwicGx0IjotMX0.Zv92RyGsh2i5dTH4txTwzYnm7Atgsz_vJICoeyTq9JY; bili_ticket_expires=1750705372; fingerprint=59eb1afd7b5ceabf7eb47b0b8017b4fc; buvid_fp=1152b7110326e8a50cebb8b660a8e069; b_lsid=BBE3109BA_19796D22539; home_feed_column=5; browser_resolution=1865-956; bmg_af_switch=1; bmg_src_def_domain=i0.hdslb.com; sid=6ijmfw26; CURRENT_FNVAL=4048; perf_dv6Tr4n=1; theme_style=light"
+    r""
 ]
 # Bilibili的COOKIE获取的方法见README.md 支持多账号
 
